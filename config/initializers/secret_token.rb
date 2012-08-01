74651d38b70c8f55f71c2f7399cd28f1236772e5
David McDonald <davmcdon@gmail.com>
2012-08-01 10:15:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseCampClone::Application.config.secret_token = 'a9c07376c282f107cd69b50fb772a87a2c574283c9206755bb1f9590e38683f0a4294d7cddc8a3e7f9060cd9d60a91ca20965c9679f2022069d2f10381ec7d31'
