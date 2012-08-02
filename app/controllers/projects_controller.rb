808b27517606b07a7f81b1631a3dfe91d2bc6849
david ladowitz <david@ladowitz.com>
2012-08-02 12:02:10 -0700

    Added public and private tags

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,9 +20,13 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:id])
+    @project.update_attributes(params[:project])
+    redirect_to(projects_path)
   end
 
   def destroy
